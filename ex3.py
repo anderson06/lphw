17ec19fdddf04a467b6cef43daea07de6baaf345
anderson06 <andersoncampos.m@gmail.com>
2015-12-17 12:39:54 -0200

    Add exercise 4

---
+++ ex3.py
@@ -0,0 +1,27 @@
+print "I will now count my chickens:"
+
+# Devides 30 by and add to 25 then print the result afte Hens
+print "Hens", 25. + 30. / 6.
+
+# Multiplies 25 by 3 then get the remaining of the division by 4, then subtract it from 100
+# then prints it next to Roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
