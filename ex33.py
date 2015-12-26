8cc3f0b134300bb05fc0b43af9b5a84c63dc34cb
anderson06 <andersoncampos.m@gmail.com>
2015-12-26 10:48:10 -0200

    Add exercise 33

---
+++ ex33.py
@@ -0,0 +1,11 @@
+print "Testing with 6 incrementing 1"
+for num in range(6):
+    print num
+
+print "Testing with 20 incrementing 2"
+for num in range(0, 20, 2):
+    print num
+
+print "Testing with -10 decrementing -2"
+for num in range(0, -10, -2):
+    print num
